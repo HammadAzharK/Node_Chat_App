a5edff4af84e6194faa8d6022436ec3e105b43b0
Waseem Akhtar <waseemahmadrock@gmail.com>
2018-11-05 14:19:05 +0500

    Add

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+
+
+
+var socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server!!!');
+
+
+    socket.emit('createMessage', {
+        from: 'Uzair Shaid',
+        text: 'Yup thats works for me'
+    });
+
+    //For Email Address
+    // socket.emit('createEmail',  {
+    //     to: 'jen@example.com',
+    //     text: 'Waseem Here...'
+    // })
+});
+
+
+socket.on('disconnect', function () {
+    console.log('Disconnected to server!!!');
+});
+
+
+socket.on('newEmail', function(email){
+    console.log('New Email', email);
+})
+
+socket.on('newMessage' , function(message){
+    console.log('newMessage', message);
+})
--- server/server.js
+++ server/server.js
@@ -1,12 +1,49 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port  = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+io.on('connection', (socket) => {
+    console.log('New User Connected');
+    
+
+    socket.emit('newMessage', {
+        from: 'John',
+        text: 'See you there...',
+        createAt: 123123
+    });
+
+    //Email Socket Emit
+    // socket.emit('newEmail', {
+    //     from: 'johnDoe@example.com',
+    //     text: 'Hey. Whats up',
+    //     createAt: 123,
+    // });
+
+    //ForMessage
+     socket.on('createMessage', (message) => {
+         console.log('createMessage', message);
+     });
+
+    //For EMail Address
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
+    socket.on("disconnect", ()=>{
+        console.log("User was diconnected")
+    })
+
+});
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 });
